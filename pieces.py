8966d0da680a0264dd9a05e3b78b9fe2f23ec5ca
stajama <stajama@yahoo.com>
2018-02-10 16:08:44 -0600

    first commit

---
+++ pieces.py
@@ -0,0 +1,11 @@
+class Piece(object):
+    """Class for all pieces in the game."""
+    def __init__(self, player, arg=None, ):
+        self.type = arg
+        self.team = player
+        self.pos =
+
+    def move(self):
+
+
+        
